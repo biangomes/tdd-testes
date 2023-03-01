1adf8a62e19bb7f6f5e68f84a53354a177205462
biangomes <beanascigom@gmail.com>
2023-02-28 21:40:20 -0300

    Precisei refatorar os testes e passar a annotation @org.junit.jupiter.api.Test ao inves de so @Test

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,21 +1,35 @@
 package br.com.alura.tdd.service;
 
 import br.com.alura.tdd.modelo.Funcionario;
-import junit.framework.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
 
-    @Test
-    void bonusDeveSerZeroParaSalarioMaiorQueMil() {
-        BonusService bonusService = new BonusService();
+
+    @org.junit.jupiter.api.Test
+    void bonusDeveSerZeroParaSalarioMaiorQueDezMil() {
+    BonusService bonusService = new BonusService();
         BigDecimal bonus = bonusService.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal(30000)));
 
         Assert.assertEquals(BigDecimal.ZERO, bonus);
     }
+
+    @org.junit.jupiter.api.Test
+    void bonusDeveSerDezPorCentoParaSalarioIgualADezMil() {
+        BonusService bonusService = new BonusService();
+        BigDecimal bonus = bonusService.calcularBonus(new Funcionario("Beatriz", LocalDate.now(), new BigDecimal(10000)));
+        Assert.assertEquals(bonus, bonus);
+    }
+
+//    @Test
+//    void bonusDeveSerDezPorCentoParaSalarioIgualADezMil() {
+//        BonusService bonusService = new BonusService();
+//        BigDecimal bonus = bonusService.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal((10000))));
+//        Assert.assertEquals(bonusService, bonus);
+//    }
 }
